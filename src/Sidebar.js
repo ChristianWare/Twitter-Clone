00ba3b314d646b04f9643f92e1d73b4e59b1d649
Chris <chris.ware.dev@gmail.com>
2021-06-17 19:55:39 -0700

    sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      {/* {Twitter Icon} */}
+      <TwitterIcon />
+      {/* {Sidebar option} */}
+      {/* {Sidebar option} */}
+      {/* {Sidebar option} */}
+      {/* {Sidebar option} */}
+
+      {/* {Button -> Tweet} */}
+    </div>
+  );
+};
+
+export default Sidebar;
