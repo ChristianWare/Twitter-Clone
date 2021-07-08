98e239487ccaede464efca92bbc9101384a044fb
Chris <chris.ware.dev@gmail.com>
2021-07-07 18:32:18 -0700

    Initial Elements

--- src/Feed.js
+++ src/Feed.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tweetbox from "./Tweetbox";
 import "./Feed.css";
 import Post from "./Post";
+import db from "./firebase";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
   return (
     <div className='feed'>
-      {/* {Header}    */}
       <div className='feed__header'>
         <h2>Home</h2>
       </div>
 
-      {/* {TweetBox}    */}
       <Tweetbox />
 
-      {/* {Post}    */}
-      <Post
-        displayName='Chris Ware'
-        username='chriswarereact'
-        verified={true}
-        text='hey it is working'
-        avatart='https://images.unsplash.com/photo-1595429035839-c99c298ffdde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
-        image='https://media0.giphy.com/media/WZdlab0U3AkSc/giphy.gif'
-      />
-    
-      {/* {Post}    */}
-      {/* {Post}    */}
-      {/* {Post}    */}
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMnIMmL1wmb6MBiD6MSRWvRmk82oKCpTM",
-  authDomain: "twitter-clone-f3830.firebaseapp.com",
-  projectId: "twitter-clone-f3830",
-  storageBucket: "twitter-clone-f3830.appspot.com",
-  messagingSenderId: "432964007139",
-  appId: "1:432964007139:web:524405e11d10d047f1c1d6",
-  measurementId: "G-FSD49K40VY",
+  authDomain: "twitter-clone-1faac.firebaseapp.com",
+  databaseURL: "https://twitter-clone-1faac.firebaseio.com",
+  projectId: "twitter-clone-1faac",
+  storageBucket: "twitter-clone-1faac.appspot.com",
+  messagingSenderId: "316208638057",
+  appId: "1:316208638057:web:75a37bf24fab32ff145af8",
+  measurementId: "G-HGFP2LDXP9",
 };
 
-const firebase = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 
